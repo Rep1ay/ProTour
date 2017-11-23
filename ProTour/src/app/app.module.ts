98c52c28ab0cd9bf79a82346766d7a526fbc18be
Alex <90spirit8@gmail.com>
2017-11-23 11:19:30 +0100

    Added components 'dash','table'

--- ProTour/src/app/app.module.ts
+++ ProTour/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyCompComponent } from './my-comp/my-comp.component';
+import { TableComponent } from './table/table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCompComponent
+    MyCompComponent,
+    TableComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
